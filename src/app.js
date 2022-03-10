685e0ce3888107460e17426b5e4aed9c39860dc4
Elsa Guillet <elsaguillet017@gmail.com>
2022-03-10 11:35:13 +0100

    initialCommit

---
+++ src/app.js
@@ -0,0 +1,120 @@
+function $(selector, el) { 
+  if (!el) {el = document;} 
+  return el.querySelector(selector); 
+} 
+function $$(selector, el) { 
+  if (!el) {el = document;} 
+  return Array.prototype.slice.call(el.querySelectorAll(selector)); 
+}
+
+window.addEventListener('DOMContentLoaded', function(){
+/*Réglages menu */
+const menu = $(".header__menu");
+const menuItems = $$(".menu__item");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    $(".menu__close").style.display = "none";
+    $(".menu__icon").style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    $(".menu__close").style.display = "block";
+    $(".menu__icon").style.display = "none";
+  }
+}
+
+$(".hamburger").addEventListener("click", toggleMenu);
+
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
+/* Compte à rebours */
+function compte_a_rebours()
+
+{
+
+    let compte_a_rebours = $(".timer");
+
+    let date_actuelle = new Date();
+    let date_fin = new Date("April 10 00:00:00 2022");
+
+    let total_secondes = (date_evenement - date_actuelle) / 1000;
+
+    let prefixe = "Fin de cette collection dans ";
+
+    if (total_secondes < 0){
+        prefixe = "Vente terminée il y a "; 
+        total_secondes = Math.abs(total_secondes); 
+    }
+
+    if (total_secondes > 0){
+      let jours = Math.floor(total_secondes / (60 * 60 * 24));
+      let heures = Math.floor((total_secondes - (jours * 60 * 60 * 24)) / (60 * 60));
+          minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+          secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
+      
+      let et = "et";
+      let mot_jour = "jours,";
+      let mot_heure = "heures,";
+      let mot_minute = "minutes,";
+      let mot_seconde = "secondes";
+
+    if (jours == 0){
+      jours = '';
+      mot_jour = '';
+    }
+    else if (jours == 1){
+      mot_jour = "jour,";
+    }
+
+    if (heures == 0){
+      heures = '';
+      mot_heure = '';
+    }
+    else if (heures == 1){
+      mot_heure = "heure,";
+    }
+
+    if (minutes == 0){
+      minutes = '';
+      mot_minute = '';
+    }
+    else if (minutes == 1){
+      mot_minute = "minute,";
+    }
+
+    if (secondes == 0){
+      secondes = '';
+      mot_seconde = '';
+      et = '';
+    }
+    else if (secondes == 1){
+      mot_seconde = "seconde";
+    }
+
+    if (minutes == 0 && heures == 0 && jours == 0){
+      et = "";
+    }
+
+    compte_a_rebours.innerHTML = prefixe + jours + ' ' + mot_jour + ' ' + heures + ' ' + mot_heure + ' ' + minutes + ' ' + mot_minute + ' ' + et + ' ' + secondes + ' ' + mot_seconde;
+  }
+
+    else
+
+    {
+
+        compte_a_rebours.innerHTML = 'Fin de la vente.';
+
+    }
+
+
+    let actualisation = setTimeout("compte_a_rebours();", 1000);
+
+}
+
+compte_a_rebours();
+});
